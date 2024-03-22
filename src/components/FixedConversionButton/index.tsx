bcb55805e53da754197ab12e25a6fa9abdf9a274
monobaba <baba@monosus.co.jp>
2024-03-22 18:11:31 +0900

    #1 fix: fixボタンの修正

--- src/components/FixedConversionButton/index.tsx
+++ src/components/FixedConversionButton/index.tsx
@@ -1,16 +1,12 @@
 const FixedConversionButton = () => {
   return (
     <div className="fixed-conversion-button">
-      <div>
-        <a className="button" href="#" target="_blank">
-          総合口座を開設する
-        </a>
-      </div>
-      <div>
-        <a className="button -nisa" href="#" target="_blank">
-          NISA口座を開設する
-        </a>
-      </div>
+      <a className="button" href="#" target="_blank">
+        総合口座を開設する
+      </a>
+      <a className="button -nisa" href="#" target="_blank">
+        NISA口座を開設する
+      </a>
     </div>
   )
 }

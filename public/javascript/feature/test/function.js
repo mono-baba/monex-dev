4d9613557fc543bdae3dd0664260363367cae007
monobaba <baba@monosus.co.jp>
2024-04-01 18:59:36 +0900

    #1 fix: スライダー作り直し完了

--- public/javascript/feature/test/function.js
+++ public/javascript/feature/test/function.js
@@ -1,170 +1,33 @@
 window.addEventListener("load", function () {
-  removeClass()
-
-  const newFunction = document.querySelector("#new-function")
-  const newFunctionRect = newFunction.getBoundingClientRect()
-  const newFucntionScrollTop = newFunctionRect.top + window.scrollY + 148
-  const sectionScrollHeight =
-    window.innerHeight - (window.innerHeight - 145 * 2) / 3
-  document.querySelectorAll('[type="radio"]').forEach(function (radio, index) {
-    radio.addEventListener("change", function () {
-      window.scrollTo({
-        top: newFucntionScrollTop + sectionScrollHeight * index,
-        behavior: "smooth", // スムーズスクロールを有効にする場合
-      })
-    })
-  })
+  initializePage()
+  addRadioChangeListeners()
 })
 let count = 0
 
-window.addEventListener("scroll", function () {
-  // 要素を取得する
-  const newFunctionInner = document.querySelector("#new-function .inner")
+// ラジオボタンの変更イベントを追加
+function addRadioChangeListeners() {
+  const radios = document.querySelectorAll('[name="slideshow"]')
+  radios.forEach(function (radio, index) {
+    radio.addEventListener("change", function () {
+      scrollToSection(index)
+    })
+  })
+}
+
+// スクロールイベントを追加
+function scrollToSection(index) {
   const newFunction = document.querySelector("#new-function")
-
-  // 要素の位置情報を取得する
-  const newFunctionInnerRect = newFunctionInner.getBoundingClientRect()
   const newFunctionRect = newFunction.getBoundingClientRect()
-  if (newFunctionInnerRect.top === 0) {
-    document.getElementById("js-poinco").classList.add("is-active")
-    const newFucntionScrollTop = newFunctionRect.top + window.scrollY + 148
-    // この間のスクロールできる高さを取得
-    const innerHeight = newFunctionRect.height - newFunctionInnerRect.height
-    // 1セクション分の高さ
-    const sectionScrollHeight =
-      window.innerHeight - (window.innerHeight - 145 * 2) / 3
-    const sectionOffset = newFunctionInner.offsetTop - 145
-    if (sectionOffset > 0 && sectionOffset <= sectionScrollHeight * 1) {
-      document.querySelector("#slide01").checked = true
-      document.querySelector('[data-slider-panel="1"]').classList.add("is-show")
-      document
-        .querySelector('[data-slider-panel="2"]')
-        .classList.remove("is-show")
-      document
-        .querySelector('[data-slider-panel="3"]')
-        .classList.remove("is-show")
-      if (count === 0) {
-        count = 1
-        document
-          .querySelector('[data-slider-panel="2"]')
-          .classList.add("is-bottom")
-        document
-          .querySelector('[data-slider-panel="2"]')
-          .classList.remove("is-top")
-        document
-          .querySelector('[data-slider-panel="3"]')
-          .classList.add("is-bottom")
-        document
-          .querySelector('[data-slider-panel="3"]')
-          .classList.remove("is-top")
-      } else if (count === 2 || count === 3) {
-        count = 1
-        document
-          .querySelector('[data-slider-panel="2"]')
-          .classList.add("is-bottom")
-        document
-          .querySelector('[data-slider-panel="2"]')
-          .classList.remove("is-top")
-        document
-          .querySelector('[data-slider-panel="3"]')
-          .classList.add("is-bottom")
-        document
-          .querySelector('[data-slider-panel="3"]')
-          .classList.remove("is-top")
-      }
-    } else if (
-      sectionOffset > sectionScrollHeight * 1 &&
-      sectionOffset <= sectionScrollHeight * 2
-    ) {
-      document.querySelector("#slide02").checked = true
-      document
-        .querySelector('[data-slider-panel="1"]')
-        .classList.remove("is-show")
-      document.querySelector('[data-slider-panel="2"]').classList.add("is-show")
-      document
-        .querySelector('[data-slider-panel="3"]')
-        .classList.remove("is-show")
-      if (count === 0 || count === 1) {
-        count = 2
-        document
-          .querySelector('[data-slider-panel="1"]')
-          .classList.add("is-top")
-        document
-          .querySelector('[data-slider-panel="1"]')
-          .classList.remove("is-bottom")
-        document
-          .querySelector('[data-slider-panel="3"]')
-          .classList.add("is-bottom")
-        document
-          .querySelector('[data-slider-panel="3"]')
-          .classList.remove("is-top")
-      } else if (count === 3) {
-        count = 2
-        document
-          .querySelector('[data-slider-panel="1"]')
-          .classList.add("is-top")
-        document
-          .querySelector('[data-slider-panel="1"]')
-          .classList.remove("is-bottom")
-        document
-          .querySelector('[data-slider-panel="3"]')
-          .classList.add("is-bottom")
-        document
-          .querySelector('[data-slider-panel="3"]')
-          .classList.remove("is-top")
-      }
-    } else if (sectionOffset > sectionScrollHeight * 2) {
-      document.querySelector("#slide03").checked = true
-      document
-        .querySelector('[data-slider-panel="1"]')
-        .classList.remove("is-show")
-      document
-        .querySelector('[data-slider-panel="2"]')
-        .classList.remove("is-show")
-      document.querySelector('[data-slider-panel="3"]').classList.add("is-show")
-      if (count === 0 || count === 1 || count === 2) {
-        count = 3
-        document
-          .querySelector('[data-slider-panel="1"]')
-          .classList.add("is-top")
-        document
-          .querySelector('[data-slider-panel="1"]')
-          .classList.remove("is-bottom")
-        document
-          .querySelector('[data-slider-panel="2"]')
-          .classList.add("is-top")
-        document
-          .querySelector('[data-slider-panel="2"]')
-          .classList.remove("is-bottom")
-      } else if (count === 4) {
-        count = 3
-        document
-          .querySelector('[data-slider-panel="1"]')
-          .classList.add("is-top")
-        document
-          .querySelector('[data-slider-panel="1"]')
-          .classList.remove("is-bottom")
-        document
-          .querySelector('[data-slider-panel="2"]')
-          .classList.add("is-top")
-        document
-          .querySelector('[data-slider-panel="2"]')
-          .classList.remove("is-bottom")
-      }
-    }
-  } else {
-    document.getElementById("js-poinco").classList.remove("is-active")
-    if (newFunctionInnerRect.top < 0) {
-      count = 4
-      document.querySelector("#slide03").checked = true
-    } else if (newFunctionInnerRect.top > 0) {
-      count = 0
-      document.querySelector("#slide01").checked = true
-    }
-  }
-})
+  const newFunctionInnerTop = window.scrollY + newFunctionRect.top + 148
+  const sectionScrollHeight = window.innerHeight + window.innerHeight / 3
+  window.scrollTo({
+    top: Math.round(newFunctionInnerTop + sectionScrollHeight * index),
+    behavior: "smooth",
+  })
+}
 
-function removeClass() {
+// ハンバーガーメニューのリンクをクリックしたときの処理
+function initializePage() {
   // .js-gnavi-linkに対するクリックイベントのリスナーを設定
   document.querySelectorAll(".js-gnavi-link").forEach(function (element) {
     element.addEventListener("click", function () {
@@ -176,4 +39,64 @@ function removeClass() {
         })
     })
   })
+}
+
+window.addEventListener("scroll", function () {
+  handlePanelVisibility()
+  handlePoincoVisibility()
+})
+
+function handlePanelVisibility() {
+  const newFunction = document.querySelector("#new-function")
+  const newFunctionRect = newFunction.getBoundingClientRect()
+  // サイト上部からnewFunctionInnerまでの距離
+  // （newFunctionInnerはabsoluteでtopの高さが0になるため、親要素からpaddingとborderを足してinnerのtopの位置を取得する）
+  const newFunctionInnerTop = window.scrollY + newFunctionRect.top + 148
+  // data-slider-panelをすべて取得
+  const panels = document.querySelectorAll("[data-slider-panel]")
+  // data-slider-panelの数を取得
+  const panelCount = panels.length
+  // 親要素に対して、panel1枚あたりの高さを取得
+  // (スクロールに余裕を持たせるため親要素はpanelの数+100vhしているため、追加分の100vhをpanel数で引く。)
+  const sectionScrollHeight =
+    window.innerHeight + window.innerHeight / panelCount
+  panels.forEach(function (panel, index) {
+    const panelIsVisible =
+      newFunctionInnerTop + sectionScrollHeight * index <= window.scrollY &&
+      newFunctionInnerTop + sectionScrollHeight * (index + 1) > window.scrollY
+    if (panelIsVisible) {
+      count = index + 1
+      document.querySelector("#slide0" + (index + 1)).checked = true
+      panel.classList.add("is-show")
+      if (index > 0) {
+        panels[index - 1].classList.add("is-top")
+        panels[index - 1].classList.remove("is-bottom")
+      }
+      if (index < panelCount - 1) {
+        panels[index + 1].classList.add("is-bottom")
+        panels[index + 1].classList.remove("is-top")
+      }
+    } else {
+      panel.classList.remove("is-show")
+    }
+  })
+}
+
+function handlePoincoVisibility() {
+  // 要素を取得する
+  const newFunctionInner = document.querySelector("#new-function .inner")
+  // 要素の位置情報を取得する
+  const newFunctionInnerRect = newFunctionInner.getBoundingClientRect()
+  if (newFunctionInnerRect.top === 0) {
+    document.getElementById("js-poinco").classList.add("is-active")
+  } else {
+    document.getElementById("js-poinco").classList.remove("is-active")
+    if (newFunctionInnerRect.top < 0) {
+      count = 4
+      document.querySelector("#slide03").checked = true
+    } else if (newFunctionInnerRect.top > 0) {
+      count = 0
+      document.querySelector("#slide01").checked = true
+    }
+  }
 }

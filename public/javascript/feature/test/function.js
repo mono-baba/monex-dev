545e03824543008d24d378083ed79cc457660ad9
monobaba <baba@monosus.co.jp>
2024-04-01 14:40:02 +0900

    #1 fix: スライダーの作り方修正途中

--- public/javascript/feature/test/function.js
+++ public/javascript/feature/test/function.js
@@ -1,10 +1,23 @@
 window.addEventListener("load", function () {
   removeClass()
+
+  const newFunction = document.querySelector("#new-function")
+  const newFunctionRect = newFunction.getBoundingClientRect()
+  const newFucntionScrollTop = newFunctionRect.top + window.scrollY + 148
+  const sectionScrollHeight =
+    window.innerHeight - (window.innerHeight - 145 * 2) / 3
+  document.querySelectorAll('[type="radio"]').forEach(function (radio, index) {
+    radio.addEventListener("change", function () {
+      window.scrollTo({
+        top: newFucntionScrollTop + sectionScrollHeight * index,
+        behavior: "smooth", // スムーズスクロールを有効にする場合
+      })
+    })
+  })
 })
 let count = 0
 
 window.addEventListener("scroll", function () {
-  addClass()
   // 要素を取得する
   const newFunctionInner = document.querySelector("#new-function .inner")
   const newFunction = document.querySelector("#new-function")
@@ -13,6 +26,8 @@ window.addEventListener("scroll", function () {
   const newFunctionInnerRect = newFunctionInner.getBoundingClientRect()
   const newFunctionRect = newFunction.getBoundingClientRect()
   if (newFunctionInnerRect.top === 0) {
+    document.getElementById("js-poinco").classList.add("is-active")
+    const newFucntionScrollTop = newFunctionRect.top + window.scrollY + 148
     // この間のスクロールできる高さを取得
     const innerHeight = newFunctionRect.height - newFunctionInnerRect.height
     // 1セクション分の高さ
@@ -28,12 +43,7 @@ window.addEventListener("scroll", function () {
       document
         .querySelector('[data-slider-panel="3"]')
         .classList.remove("is-show")
-      console.log(count)
       if (count === 0) {
-        console.log("上からきた1番目")
-        count = 1
-      } else if (count === 2 || count === 3) {
-        console.log("下からきた1番目")
         count = 1
         document
           .querySelector('[data-slider-panel="2"]')
@@ -41,12 +51,31 @@ window.addEventListener("scroll", function () {
         document
           .querySelector('[data-slider-panel="2"]')
           .classList.remove("is-top")
+        document
+          .querySelector('[data-slider-panel="3"]')
+          .classList.add("is-bottom")
+        document
+          .querySelector('[data-slider-panel="3"]')
+          .classList.remove("is-top")
+      } else if (count === 2 || count === 3) {
+        count = 1
+        document
+          .querySelector('[data-slider-panel="2"]')
+          .classList.add("is-bottom")
+        document
+          .querySelector('[data-slider-panel="2"]')
+          .classList.remove("is-top")
+        document
+          .querySelector('[data-slider-panel="3"]')
+          .classList.add("is-bottom")
+        document
+          .querySelector('[data-slider-panel="3"]')
+          .classList.remove("is-top")
       }
     } else if (
       sectionOffset > sectionScrollHeight * 1 &&
       sectionOffset <= sectionScrollHeight * 2
     ) {
-      console.log(count)
       document.querySelector("#slide02").checked = true
       document
         .querySelector('[data-slider-panel="1"]')
@@ -56,7 +85,6 @@ window.addEventListener("scroll", function () {
         .querySelector('[data-slider-panel="3"]')
         .classList.remove("is-show")
       if (count === 0 || count === 1) {
-        console.log("上からきた2番目")
         count = 2
         document
           .querySelector('[data-slider-panel="1"]')
@@ -64,9 +92,20 @@ window.addEventListener("scroll", function () {
         document
           .querySelector('[data-slider-panel="1"]')
           .classList.remove("is-bottom")
+        document
+          .querySelector('[data-slider-panel="3"]')
+          .classList.add("is-bottom")
+        document
+          .querySelector('[data-slider-panel="3"]')
+          .classList.remove("is-top")
       } else if (count === 3) {
-        console.log("下からきた2番目")
         count = 2
+        document
+          .querySelector('[data-slider-panel="1"]')
+          .classList.add("is-top")
+        document
+          .querySelector('[data-slider-panel="1"]')
+          .classList.remove("is-bottom")
         document
           .querySelector('[data-slider-panel="3"]')
           .classList.add("is-bottom")
@@ -75,7 +114,6 @@ window.addEventListener("scroll", function () {
           .classList.remove("is-top")
       }
     } else if (sectionOffset > sectionScrollHeight * 2) {
-      console.log(count)
       document.querySelector("#slide03").checked = true
       document
         .querySelector('[data-slider-panel="1"]')
@@ -85,8 +123,13 @@ window.addEventListener("scroll", function () {
         .classList.remove("is-show")
       document.querySelector('[data-slider-panel="3"]').classList.add("is-show")
       if (count === 0 || count === 1 || count === 2) {
-        console.log("上からきた3番目")
         count = 3
+        document
+          .querySelector('[data-slider-panel="1"]')
+          .classList.add("is-top")
+        document
+          .querySelector('[data-slider-panel="1"]')
+          .classList.remove("is-bottom")
         document
           .querySelector('[data-slider-panel="2"]')
           .classList.add("is-top")
@@ -94,8 +137,13 @@ window.addEventListener("scroll", function () {
           .querySelector('[data-slider-panel="2"]')
           .classList.remove("is-bottom")
       } else if (count === 4) {
-        console.log("下からきた3番目")
         count = 3
+        document
+          .querySelector('[data-slider-panel="1"]')
+          .classList.add("is-top")
+        document
+          .querySelector('[data-slider-panel="1"]')
+          .classList.remove("is-bottom")
         document
           .querySelector('[data-slider-panel="2"]')
           .classList.add("is-top")
@@ -105,41 +153,13 @@ window.addEventListener("scroll", function () {
       }
     }
   } else {
-    console.log("out of view")
-    if (count === 3) {
+    document.getElementById("js-poinco").classList.remove("is-active")
+    if (newFunctionInnerRect.top < 0) {
       count = 4
-      document.querySelector('[data-slider-panel="1"]').classList.add("is-top")
-      document
-        .querySelector('[data-slider-panel="1"]')
-        .classList.remove("is-bottom")
-      document.querySelector('[data-slider-panel="2"]').classList.add("is-top")
-      document
-        .querySelector('[data-slider-panel="2"]')
-        .classList.remove("is-bottom")
-      document.querySelector('[data-slider-panel="3"]').classList.add("is-top")
-      document
-        .querySelector('[data-slider-panel="3"]')
-        .classList.remove("is-bottom")
-    } else if ((count === 1, count === 0)) {
+      document.querySelector("#slide03").checked = true
+    } else if (newFunctionInnerRect.top > 0) {
       count = 0
-      document
-        .querySelector('[data-slider-panel="1"]')
-        .classList.add("is-bottom")
-      document
-        .querySelector('[data-slider-panel="1"]')
-        .classList.remove("is-top")
-      document
-        .querySelector('[data-slider-panel="2"]')
-        .classList.add("is-bottom")
-      document
-        .querySelector('[data-slider-panel="2"]')
-        .classList.remove("is-top")
-      document
-        .querySelector('[data-slider-panel="3"]')
-        .classList.add("is-bottom")
-      document
-        .querySelector('[data-slider-panel="3"]')
-        .classList.remove("is-top")
+      document.querySelector("#slide01").checked = true
     }
   }
 })
@@ -156,19 +176,4 @@ function removeClass() {
         })
     })
   })
-}
-
-function addClass() {
-  var element = document.getElementById("js-poinco")
-  var rect = element.getBoundingClientRect()
-  var windowHeight = window.innerHeight || document.documentElement.clientHeight
-
-  // 要素が画面の中央にあるかどうかを検出
-  if (rect.top <= windowHeight / 2 && rect.bottom >= windowHeight / 2) {
-    // 画面中央にある場合
-    element.classList.add("is-active")
-  } else {
-    // 画面中央にない場合
-    element.classList.remove("is-active")
-  }
 }
